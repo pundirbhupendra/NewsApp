d76c1fbf6b0b45d256049fa21816df2df2bf15c6
bhupendra <37104937+pundirbhupendra@users.noreply.github.com>
2026-01-26 15:40:12 +0530

    dev app config added

---
+++ app/src/dev/kotlin/com/portfolio/newsapp/config/AppConfig.kt
@@ -0,0 +1,6 @@
+package com.portfolio.newsapp.config
+object AppConfig {
+    const val BASE_URL = "https://newsapi.org/v2/"
+    const val API_KEY = "YOUR_DEV_API_KEY_HERE"
+    const val ENVIRONMENT = "DEV"
+}
